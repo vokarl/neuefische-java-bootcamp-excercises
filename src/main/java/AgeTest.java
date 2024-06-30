8475f9e8d3bac94785b5991b73cff2ff47961aab
vokarl <karl.vogl@gmx.de>
2024-06-30 19:53:11 +0200

    Setup project

---
+++ src/main/java/AgeTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AgeTest {
+    public static void main(String[] args) {
+        Scanner scanObject = new Scanner(System.in);
+        String greeting = "Hello World";
+
+String firstName = "Karl";
+ System.out.println(greeting +" my name is " + firstName);
+
+ System.out.println("Enter your age: ");
+ int userAge = scanObject.nextInt();
+
+ if (userAge >= 30) {
+     System.out.println("You are in");
+ }else if (userAge <= 30){
+     System.out.println("You are out");
+  }
+    else  {
+            System.out.println("You are definaltey out");
+}
+    //
+}}
+
+
+
+
