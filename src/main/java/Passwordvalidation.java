3d798ee0e21f96d3704c75e0982b72f6a0674988
vokarl <karl.vogl@gmx.de>
2024-07-02 11:47:38 +0200

    add length validation and testing

---
+++ src/main/java/Passwordvalidation.java
@@ -0,0 +1,10 @@
+public class Passwordvalidation {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static boolean lengthValidation(String password) {
+        return password.length() > 7;
+    }
+}
+
---
+++ src/test/java/PasswordvalidationTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationTest {
+
+    @Test
+    public void lengthValidationTest_true_when_bigger7() {
+        //GIVEN
+        String password = "1234567";
+        boolean expected = false;
+
+        //WHEN
+        boolean actual = Passwordvalidation.lengthValidation(password);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void otherTest_true_when_bigger20() {
+        //GIVEN
+        String password = "12345678901234567890";
+        boolean expected = true;
+
+        //WHEN
+        boolean actual =Passwordvalidation.lengthValidation(password);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+}
