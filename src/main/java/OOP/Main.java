a6a512b62412410d9abfd826070dfca118a970bc
vokarl <karl.vogl@gmx.de>
2024-07-07 12:35:15 +0200

    add Main to car project in oop package

---
+++ src/main/java/OOP/Main.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Hyunday", "Nexo","blue",2023 );
+        myCar.start();
+        myCar.speed(78);
+    }
+}
